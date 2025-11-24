0147c997099baf046f03061c161e7e9e30d017cf
Rohan-Rawat7508 <rohan.rawat@diligenticinnovations.com>
2025-11-24 15:06:50 +0530

    chore: create habit screen added

--- habit-tracker-app/app/navigation/AppNavigator.tsx
+++ habit-tracker-app/app/navigation/AppNavigator.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../screens/Home';
 import { SCREENS } from '../utils/routes';
+import CreateHabit from '../screens/CreateHabit';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function AppNavigator() {
     <NavigationContainer>
      <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen component={Home} name={SCREENS.Home}/>
+      <Stack.Screen component={CreateHabit} name={SCREENS.CreateHabit}/>
      </Stack.Navigator>
     </NavigationContainer>
   )
