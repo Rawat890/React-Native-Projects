74bfa174eb6c5ce4ccc2a6e06c2c667182045d09
Rawat890 <rohanrawat7508@gmail.com>
2025-11-25 11:44:19 +0530

    chore: email validation added

---
+++ habit-tracker-app/app/utils/validators.ts
@@ -0,0 +1,6 @@
+export const validateEmail = (value: string): string => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!value) return 'Email is required';
+    if (!regex.test(value)) return 'Invalid email';
+    return '';
+  };
