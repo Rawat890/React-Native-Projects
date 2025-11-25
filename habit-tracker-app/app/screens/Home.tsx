880a44a65e4b786746cd6d04c1cabef030f6457c
Rawat890 <rohanrawat7508@gmail.com>
2025-11-25 11:43:09 +0530

    chore: navigation added to create habit screen

--- habit-tracker-app/app/screens/Home.tsx
+++ habit-tracker-app/app/screens/Home.tsx
@@ -1,43 +1,45 @@
-import { View, Text, ScrollView, StyleSheet, Pressable } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import { AntDesign, Ionicons } from '@expo/vector-icons';
 import { COLORS } from '../utils/colors';
 import TextButton from '../components/TextButton';
 import { scale } from 'react-native-size-matters';
+import { useNavigation } from '@react-navigation/native';
+import { SCREENS } from '../utils/routes';
 
 export default function Home() {
   const [option, setOption] = useState('Today');
-
+  const navigation = useNavigation();
+  
+  const createHabit = () =>{
+    navigation.navigate(SCREENS.CreateHabit);
+  }
   return (
-    <ScrollView>
+    <View style={{flex:1, backgroundColor: COLORS.white}}>
       <View style={styles.iconContainer}>
         <Ionicons name='logo-foursquare' size={24} color={COLORS.black} />
         <Text style={styles.text}>Habit Tracking App</Text>
-
-        <AntDesign name='plus' size={24} color={COLORS.black} />
+        <AntDesign name='plus' size={24} color={COLORS.black} onPress={createHabit}/>
       </View>
       <View style={styles.buttonContainer}>
         <TextButton
           title='Today'
           backgroundColor={option === "Today" ? COLORS.primary : COLORS.gray300}
-          style={styles.button}
           onPress={() => setOption('Today')}
 
         />
         <TextButton
           title='Weekly'
           backgroundColor={option === "Weekly" ? COLORS.secondary : COLORS.gray300}
-          style={styles.button}
           onPress={() => setOption('Weekly')}
         />
         <TextButton
           title='Overall'
           backgroundColor={option === "Overall" ? COLORS.success : COLORS.gray300}
-          style={styles.button}
           onPress={() => setOption('Overall')}
         />
       </View>
-    </ScrollView>
+    </View>
   )
 }
 
@@ -46,15 +48,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: scale(10),
-    marginHorizontal: scale(10)
+    marginHorizontal: scale(10),
+    alignItems:'center',
   },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    marginTop: scale(10)
-  },
-  button: {
-    marginVertical: scale(5),
+    marginTop: scale(20)
   },
   text: {
     fontSize: scale(20),
