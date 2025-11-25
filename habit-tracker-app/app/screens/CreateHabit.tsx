7ba282ea4ec0db9a5dfadd46d21d0eeab428e7cf
Rawat890 <rohanrawat7508@gmail.com>
2025-11-25 12:08:36 +0530

    chore: file updated

--- habit-tracker-app/app/screens/CreateHabit.tsx
+++ habit-tracker-app/app/screens/CreateHabit.tsx
@@ -9,58 +9,65 @@ import { days } from '../utils/others'
 
 export default function CreateHabit() {
   return (
-    <View style={styles.container}>
-      <Ionicons name='arrow-back' size={24} color={COLORS.black} />
-      <Text>Create Habit</Text>
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={styles.container}>
+        <Ionicons name='arrow-back' size={24} color={COLORS.black} />
+        <Text style={styles.normalText}>Create Habit</Text>
 
-      <View style={styles.input}>
-        <InputWithIcon
-          label="Habit"
-          placeholder="Enter your habit..."
-          keyboardType="email-address"
-        />
-      </View>
-
-      <View>
-        <Text>Color</Text>
-        <View style={styles.colorContainer}>
-          {
-            habitColors.map((item, index) => (
-              <TouchableOpacity key={index}>
-                <FontAwesome name='square' size={30} color={item} />
-              </TouchableOpacity>
-            ))
-          }
-        </View>
-      </View>
-
-      <View>
-        <Text>Repeat</Text>
-        <View style={styles.btnContainer}>
-          <TextButton
-            title='Daily'
-            style={styles.button}
-          />
-          <TextButton
-            title='Weekly'
-            style={styles.button}
+        <View style={styles.input}>
+          <InputWithIcon
+            label="Habit"
+            placeholder="Enter your habit..."
+            keyboardType="email-address"
           />
         </View>
-      </View>
 
-      <View style={styles.week}>
-        {
-          days.map((item, index) => (
+        <View>
+          <Text style={styles.normalText}>Color</Text>
+          <View style={styles.colorContainer}>
+            {
+              habitColors.map((item, index) => (
+                <TouchableOpacity key={index} style={{ marginTop: scale(4) }}>
+                  <FontAwesome name='square' size={30} color={item} />
+                </TouchableOpacity>
+              ))
+            }
+          </View>
+        </View>
+
+        <View style={{ marginTop: scale(10) }}>
+          <Text style={styles.normalText}>Repeat</Text>
+          <View style={styles.btnContainer}>
             <TextButton
-              key={index}
-              title={item}
-              backgroundColor={COLORS.gray300}
-              textStyle={{ color: COLORS.black }}
-              style={styles.weekButton}
+              title='Daily'
+              style={styles.button}
             />
+            <TextButton
+              title='Weekly'
+              style={styles.button}
+            />
+          </View>
+        </View>
 
-          ))
-        }
+        <View style={{ marginTop: scale(10) }}>
+          <Text style={styles.normalText}>On These days</Text>
+          <View style={styles.week}>
+            {
+              days.map((item, index) => (
+                <TouchableOpacity key={index} style={styles.weekButton}>
+                  <Text>{item}</Text>
+                </TouchableOpacity>
+
+              ))
+            }
+          </View>
+        </View>
+
+        <View style={{marginTop: scale(10)}}>
+          <Text style={styles.normalText}>Reminder</Text>
+        </View>
+
+        <TextButton title='Save Habit' backgroundColor={COLORS.success} style={{marginTop: scale(20), marginHorizontal: scale(0)}}/>
       </View>
     </View>
   )
@@ -69,14 +76,15 @@ export default function CreateHabit() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: COLORS.white
+    backgroundColor: COLORS.white,
+    marginHorizontal: scale(10)
   },
   input: {
     marginHorizontal: scale(10),
   },
   colorContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-evenly'
+    gap: scale(10)
   },
   btnContainer: {
     flexDirection: 'row',
@@ -84,16 +92,23 @@ const styles = StyleSheet.create({
     marginTop: scale(10),
   },
   button: {
-    width: scale(160)
+    width: scale(150)
   },
   week: {
     flexDirection: 'row',
-    gap: scale(5),
+    gap: scale(10),
     marginTop: scale(10),
   },
   weekButton: {
-    width: scale(106),
+    width: scale(36),
     height: scale(36),
     borderRadius: scale(8),
+    backgroundColor: COLORS.gray200,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  normalText: {
+    fontSize: scale(14),
+    fontWeight: '400',
   }
 })
