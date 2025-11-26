f14fe1447b98e2342dfc9684b1153786308d6130
Rawat890 <rohanrawat7508@gmail.com>
2025-11-26 11:46:09 +0530

    chore: new end points added to fetch list and set completed

--- habit-tracker-app/api/index.js
+++ habit-tracker-app/api/index.js
@@ -40,3 +40,37 @@ app.post("/create-habit", async (req, res) => {
     res.status(500).json({error: "Network error occured"})
   }
 })
+
+//endpoint to fecth list of habits in backend
+
+app.get("/habitsList", async (req, res) => {
+  try {
+    const habitsList = await Habit.find({});
+
+    res.status(200).json(habitsList);
+  } catch (error) {
+    res.status(500).json({error: "Error occured while fetching habits"})
+  }
+})
+
+app.put()
+
+
+app.put("/create-habit/:habitId/completed/:day", async (req, res) => {
+  try {
+
+    const {habitId, day} =req.params;
+    const habit = await Habit.findById(habitId);
+
+    if (!habit) {
+      return res.status(404).json({error: "Habit not found"});
+    }
+
+    habit.completed[day] = true;
+    await habit.save()
+    res.status(200).json(habit);
+
+  } catch (error) {
+    res.status(500).json({error: "Error occcured while updating habit completion"})
+  }
+})
