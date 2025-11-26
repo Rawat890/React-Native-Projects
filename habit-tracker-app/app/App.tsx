e74fefec39fea361d0203fcd280329b982c0e937
Rawat890 <rohanrawat7508@gmail.com>
2025-11-26 17:25:17 +0530

    chore: safe area removed

--- habit-tracker-app/app/App.tsx
+++ habit-tracker-app/app/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import AppNavigator from './navigation/AppNavigator';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <AppNavigator />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <AppNavigator />
   )
 }
 
