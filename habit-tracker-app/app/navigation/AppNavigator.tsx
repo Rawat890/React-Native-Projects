a755e1ca9d93822f58f4c2a4205f2bd5856a9891
Rawat890 <rohanrawat7508@gmail.com>
2025-11-26 11:23:28 +0530

    chore: navigation ref passed

--- habit-tracker-app/app/navigation/AppNavigator.tsx
+++ habit-tracker-app/app/navigation/AppNavigator.tsx
@@ -4,12 +4,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../screens/Home';
 import { SCREENS } from '../utils/routes';
 import CreateHabit from '../screens/CreateHabit';
+import { navigationRef } from '../utils/navigationService';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
      <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen component={Home} name={SCREENS.Home}/>
       <Stack.Screen component={CreateHabit} name={SCREENS.CreateHabit}/>
