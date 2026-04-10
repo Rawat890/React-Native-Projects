00370f6c908607c77413579d23f622daacbb2a75
Rawat890 <rohanrawat7508@gmail.com>
2026-04-11 04:45:05 +0530

    chore: color constant file created

---
+++ healthCareApp/src/utils/colors.ts
@@ -0,0 +1,37 @@
+export const COLORS = {
+  white: "#FFFFFF",
+
+  black: "#000000",
+  lightBlack: "#333333",
+  mediumBlack: "#555555",
+  darkBlack: "#111111",
+
+  grey: "#808080",
+  lightGrey: "#D3D3D3",
+  mediumGrey: "#A9A9A9",
+  darkGrey: "#696969",
+
+  red: "#FF3B30",
+  green: "#34C759",
+  blue: "#007AFF",
+  yellow: "#FFCC00",
+  orange: "#FF9500",
+
+  background: "#F5F5F5",
+  border: "#E0E0E0",
+
+  transparent: "transparent",
+  bg: '#0d0d0f',
+  surface: '#16161a',
+  surfaceAlt: '#1c1c22',
+  accent: '#a78bfa',
+  accentSoft: '#a78bfa18',
+  text: '#f0eefc',
+  muted: '#6b6a7e',
+  dim: '#3a3a46',
+  danger: '#f87171',
+  dangerSoft: '#f8717115',
+  greenSoft: '#34d39918',
+  orangeSoft: '#fb923c18',
+  blueSoft: '#60a5fa18',
+};
