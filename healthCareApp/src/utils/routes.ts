dc302bf370f9c77882963a67ab576c205ec84c4d
Rawat890 <rohanrawat7508@gmail.com>
2026-04-11 10:21:16 +0530

    chore: routes updated

---
+++ healthCareApp/src/utils/routes.ts
@@ -0,0 +1,9 @@
+export const SCREENS = {
+  Login: 'Login',
+  Register: 'Register',
+  ForgotPassword: 'ForgotPassword',
+  Home: 'Home',
+  Pharmacy: 'Pharmacy',
+  NearBy: 'NearBy',
+  Reminder: 'Reminder',
+}
