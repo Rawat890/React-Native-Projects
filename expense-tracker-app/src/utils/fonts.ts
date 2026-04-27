d7dd5b090adfd3a3fef5c627798ee9ef245ace14
Rawat890 <rohanrawat7508@gmail.com>
2026-04-27 10:08:41 +0530

    chore: baloo fonts constant added

---
+++ expense-tracker-app/src/utils/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+  balooBold: "BalooThambi2-Bold",
+  balooExtraBold: "BalooThambi2-ExtraBold",
+  balooMedium: "BalooThambi2-Medium",
+  balooRegular: "BalooThambi2-Regular",
+  balooSemi: "BalooThambi2-Semi",
+};
