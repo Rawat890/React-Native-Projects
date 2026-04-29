e2e446ccafc0815a9111366da19d269c7d42cee4
Rawat890 <rohanrawat7508@gmail.com>
2026-04-29 10:55:24 +0530

    chore: login and register screen created

---
+++ expense-tracker-app/src/screens/Login.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { scale } from 'react-native-size-matters'
+import AnimatedInput from '../components/AnimatedInput'
+import { COLORS } from '../utils/colors'
+import { fonts } from '../utils/fonts'
+
+const Login = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputWrapper}>
+        <AnimatedInput
+          label='Email'
+          value={email}
+          onChangeText={setEmail}
+          inputContainerStyle={styles.inputContainer}
+          labelStyle={styles.labelStyle}
+          placeholder='Enter email'
+        />
+        <AnimatedInput
+          label='Password'
+          value={password}
+          onChangeText={setPassword}
+          inputContainerStyle={styles.inputContainer}
+          labelStyle={styles.labelStyle}
+          placeholder='Enter password'
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:COLORS.white
+  },
+  inputWrapper:{
+    marginHorizontal: scale(10),
+  },
+  inputContainer: {
+    padding: scale(8),
+    marginVertical: scale(10)
+  },
+  labelStyle: {
+    fontSize: scale(14),
+    fontFamily: fonts.balooMedium
+  }
+})
+
+export default Login
