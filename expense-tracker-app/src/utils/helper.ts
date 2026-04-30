043a9f899a96e6cfb3658a0e42b6dab0fd5b7886
Rawat890 <rohanrawat7508@gmail.com>
2026-04-30 09:43:24 +0530

    chore: enhance CATEGORIES with color properties and add utility functions for expense processing

--- expense-tracker-app/src/utils/helper.ts
+++ expense-tracker-app/src/utils/helper.ts
@@ -1,3 +1,4 @@
+import { COLORS } from "./colors";
 
 export const EXPENSE_COLORS = [
   "#FF3B30",
@@ -8,20 +9,146 @@ export const EXPENSE_COLORS = [
 ];
 
 export const CATEGORIES = [
-  { id: 1, name: "Food", label: "Food and snacks", icon: "🍔" },
-  { id: 2, name: "Gym and Exercise", label: "Gym", icon: "🏋️" },
-  { id: 3, name: "Books", label: "Books", icon: "📚" },
-  { id: 4, name: "Daily Use", label: "Daily uses", icon: "🧴" },
-  { id: 5, name: "Clothes", label: "Clothes", icon: "👕" },
-  { id: 6, name: "Bills/Utilities", label: "Bills/Utilities", icon: "💡" },
-  { id: 7, name: "Entertainment", label: "Entertainment", icon: "🎬" },
-  { id: 8, name: "Shopping", label: "Shopping", icon: "🛍️" },
-  { id: 9, name: "Travel", label: "Travel", icon: "✈️" },
-  { id: 10, name: "Phone/Internet", label: "Phone/Internet", icon: "📱" },
-  { id: 11, name: "Fuel", label: "Fuel", icon: "⛽" },
-  { id: 12, name: "Education", label: "Education", icon: "🎓" },
-  { id: 13, name: "Transfer", label: "Transfer", icon: "💸" },
+  {
+    id: 1,
+    name: "Food",
+    label: "Food and snacks",
+    icon: "🍔",
+    color: COLORS.blueTint,
+  },
+  {
+    id: 2,
+    name: "Gym and Exercise",
+    label: "Gym",
+    icon: "🏋️",
+    color: COLORS.red,
+  },
+  {
+    id: 3,
+    name: "Books",
+    label: "Books",
+    icon: "📚",
+    color: COLORS.accentSoft,
+  },
+  {
+    id: 4,
+    name: "Daily Use",
+    label: "Daily uses",
+    icon: "🧴",
+    color: COLORS.green,
+  },
+  {
+    id: 5,
+    name: "Clothes",
+    label: "Clothes",
+    icon: "👕",
+    color: COLORS.blueSoft,
+  },
+  {
+    id: 6,
+    name: "Bills/Utilities",
+    label: "Bills/Utilities",
+    icon: "💡",
+    color: COLORS.green,
+  },
+  {
+    id: 7,
+    name: "Entertainment",
+    label: "Entertainment",
+    icon: "🎬",
+    color: COLORS.border,
+  },
+  {
+    id: 8,
+    name: "Shopping",
+    label: "Shopping",
+    icon: "🛍️",
+    color: COLORS.green,
+  },
+  { id: 9, name: "Travel", label: "Travel", icon: "✈️", color: COLORS.green },
+  {
+    id: 10,
+    name: "Phone/Internet",
+    label: "Phone/Internet",
+    icon: "📱",
+    color: COLORS.green,
+  },
+  { id: 11, name: "Fuel", label: "Fuel", icon: "⛽", color: COLORS.green },
+  {
+    id: 12,
+    name: "Education",
+    label: "Education",
+    icon: "🎓",
+    color: COLORS.green,
+  },
+  {
+    id: 13,
+    name: "Transfer",
+    label: "Transfer",
+    icon: "💸",
+    color: COLORS.green,
+  },
   { id: 14, name: "Transportation", label: "Transportation", icon: "🚌" },
-  { id: 15, name: "Housing", label: "Housing", icon: "🏠" },
-  { id: 16, name: "Miscellaneous", label: "Miscellaneous", icon: "📦" },
+  {
+    id: 15,
+    name: "Housing",
+    label: "Housing",
+    icon: "🏠",
+    color: COLORS.green,
+  },
+  {
+    id: 16,
+    name: "Miscellaneous",
+    label: "Miscellaneous",
+    icon: "📦",
+    color: COLORS.green,
+  },
 ];
+
+export const getId = () => {
+  return Date.now().toString() + Math.floor(Math.random() * 1000).toString();
+};
+
+export const getDate = () => {
+  return new Date().toLocaleDateString("en-GB");
+};
+
+export const getCategoryColor = (color: string) => {
+  const category = CATEGORIES.find((cat) => cat.color === color);
+  return category ? category.color : COLORS.white;
+};
+
+export const processDataForChart = (expenses) => {
+  if (!Array.isArray(expenses) || EXPENSE_COLORS.length === 0) {
+    return [];
+  }
+
+  const totalSpending = expenses.reduce(
+    (acc, expense) => acc + expense.amount,
+    0,
+  );
+
+  if (totalSpending === 0) {
+    return [];
+  }
+
+  const spendingByCategory = expenses.reduce((acc, expense) => {
+    acc[expense.category] = (acc[expense.category] || 0) + expense.amount;
+    return acc;
+  }, {});
+
+  const pieChartData = Object.keys(spendingByCategory).map((categoryName) => {
+    const amount = spendingByCategory[categoryName];
+    const percentage = Math.round((amount / totalSpending) * 100);
+    const categoryInfo = CATEGORIES.find((cat) => cat.name === categoryName);
+
+    return {
+      name: categoryName,
+      amount,
+      value: percentage,
+      color: categoryInfo?.color || COLORS.white,
+      text: `${percentage}%`,
+    };
+  });
+  return pieChartData;
+};
