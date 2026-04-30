f83ed7763067e894735d4b810681c2451c42613c
Rawat890 <rohanrawat7508@gmail.com>
2026-04-30 09:45:29 +0530

    chore: Animated loader component initialised

---
+++ expense-tracker-app/src/components/AnimatedLoader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const AnimatedLoader = () => {
+  return (
+    <View>
+      <Text>AnimatedLoader</Text>
+    </View>
+  )
+}
+
+export default AnimatedLoader
